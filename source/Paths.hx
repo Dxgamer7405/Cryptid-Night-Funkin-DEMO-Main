946f2e9b51fab48d9588cf300b8c5dc37b85ef77
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2022-08-29 04:19:19 -0400

    update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -186,13 +186,13 @@ class Paths
 	}
 	inline static public function lua(key:String, ?library:String)
 	{
-	 return Main.path + getPath('$key.lua', TEXT, library);
+		return Main.path + getPath('$key.lua', TEXT, library);
 	}
 	inline static public function luaAsset(key:String, ?library:String)
   {
-	 return getPath('$key.lua', TEXT, library);
+		return getPath('$key.lua', TEXT, library);
   }
-        
+
 	static public function video(key:String)
 	{
 		#if MODS_ALLOWED
